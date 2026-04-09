c72a93fd107b1166bdc2832f497987fdeb9cbef4
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-04-09 17:24:45 +0530

    fifth problem

---
+++ 5.py
@@ -0,0 +1,9 @@
+''' Use comparison operator to find out whether ‘a’ given variable a is greater than
+‘b’ or not. Take a = 34 and b = 80'''
+
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+if a>b:
+    print(a," is greater than" ,b)
+else:
+    print(b, "is greater than", a)
