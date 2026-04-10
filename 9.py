0cdc132443f9ffe8a7436df199fc7de038ddec9c
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-04-10 19:15:54 +0530

    9th problem

---
+++ 9.py
@@ -0,0 +1,10 @@
+# Write a program to fill in a letter template given below with name and date.
+# letter = '''
+# Dear <|Name|>,
+# You are selected!
+# <|Date|>
+# '''
+
+a=input("Enter your name: ")
+
+print(f" Dear {a}\n \t You are selected!\n 10th April 2026")
