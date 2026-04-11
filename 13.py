ca53831e5f89cd05a81e39aa6b9d4495c3d97596
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-04-11 19:36:26 +0530

    13th Problem

---
+++ 13.py
@@ -0,0 +1,13 @@
+# Write a program to store seven fruits in a list entered by the user.
+
+a=input("1st Fruit: ")
+b=input("2st Fruit: ")
+c=input("3st Fruit: ")
+d=input("4st Fruit: ")
+e=input("5st Fruit: ")
+f=input("6st Fruit: ")
+j=input("7st Fruit: ")
+
+l_fruit=[a, b, c, d, e, f, j]
+print(l_fruit)
+
